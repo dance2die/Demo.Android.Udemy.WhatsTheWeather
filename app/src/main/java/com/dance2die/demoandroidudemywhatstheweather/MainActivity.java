32de1db39cb3840f84f35fdc0948180b605076d7
dance2die <dontbothermewithspam@gmail.com>
2016-04-04 07:41:19 -0400

    Retrieves correct JSON data from OpenWeatherMap

--- app/src/main/java/com/dance2die/demoandroidudemywhatstheweather/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemywhatstheweather/MainActivity.java
@@ -1,15 +1,97 @@
 package com.dance2die.demoandroidudemywhatstheweather;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String API_URL = "http://api.openweathermap.org/data/2.5/forecast/city?q=%s&APPID=286c90a2b702c059135ae9f427fbc5ac";
+
+    private EditText cityName;
+
+    public void findWeather(View view) {
+        String url = null;
+        try {
+            url = String.format(API_URL, URLEncoder.encode(cityName.getText().toString(), "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        Log.i("QUERY", url);
+
+        DownloadTask task = new DownloadTask();
+        task.execute(url);
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls)
+        {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                int data = reader.read();
+                while (data != -1){
+                    char current = (char)data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+//                String weatherInfo = jsonObject.getString("weather");
+//
+//                Log.i("Website Content", weatherInfo);
+
+                JSONArray listArray = jsonObject.getJSONArray("list");
+                for (int i = 0; i < listArray.length(); i++){
+                    JSONObject jsonPart = listArray.getJSONObject(i).getJSONArray("weather").getJSONObject(0);
+
+                    Log.i("main", jsonPart.getString("main"));
+                    Log.i("description", jsonPart.getString("description"));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +108,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        cityName = (EditText) findViewById(R.id.cityName);
     }
 
     @Override
